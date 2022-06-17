df1a168f8089a0a2f82931377ba3ab3bf69662ef
tanow <alinavetrova25@gmail.com>
2022-06-17 18:56:14 +0600

    + zadacha 1 from prakt 3

---
+++ ZdchLnk/p03_z01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p03_z01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double edge, area;
+            edge = double.Parse(Console.ReadLine());
+            area = Math.Pow(edge, 2) * 6;
+            if (edge > 0)
+            {
+                Console.WriteLine(area);
+            }
+            else
+            {
+                Console.WriteLine("Значение edge должно быть положительно");
+            }
+        }
+    }
+}
