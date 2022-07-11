724ca6210f5d3f63df70d888fa857f380044c4fd
tanow <alinavetrova25@gmail.com>
2022-07-11 06:54:22 +0600

    + 23 from 6

---
+++ ZdchLnk/p06_z23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace p06_z23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            
+            if (N < 0)
+            {
+                Console.WriteLine("Значение N должно быть неотрицательным");
+            }
+            else
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    Console.Write("!");
+                    if (i % 5 == 0)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ ZdchLnk/p06_z24/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace p06_z24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            if (N < 0)
+            {
+                Console.WriteLine("Значение N должно быть неотрицательным");
+            }
+            else
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    Console.Write("#");
+                    if (i % 20 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+            
+        }
+    }
+}
